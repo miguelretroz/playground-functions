e688f50a57cad08d358592815a800aaa3084a5ec
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-29 11:35:56 -0300

    Challenge 11-> Checking phone number conditions

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,10 +15,70 @@ function techList(tech, name) {
   return tech;
 }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function isEleven(phoneNumber) {
+  let condition = false;
+  if (phoneNumber.length === 11) {
+    condition = true;
+  }
+  return condition;
 }
+
+function isPositive(phoneNumber) {
+  let result = true;
+  for (let number of phoneNumber) {
+    if (number < 0) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+function isNotTen(phoneNumber) {
+  let result = true;
+  for (let number of phoneNumber) {
+    if (number >= 10) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+function numbersCount(phoneNumber) {
+  let result = true;
+  for (let number of phoneNumber) {
+    let count = 0;
+    for (let number02 of phoneNumber) {
+      if (number === number02) {
+        count += 1;
+      }
+    }
+    if (count >= 4) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+// Desafio 11
+function generatePhoneNumber(phoneNumber) {
+  // seu código aqui
+  let result;
+  if (isEleven(phoneNumber)) {
+    if (isPositive(phoneNumber) && isNotTen(phoneNumber) && numbersCount(phoneNumber)) {
+      
+    } else {
+      result = 'não é possível gerar um número de telefone com esses valores';
+    }
+  } else {
+    result = 'Array com tamanho incorreto.';
+  }
+  return result;
+}
+
+console.log(generatePhoneNumber([ 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
